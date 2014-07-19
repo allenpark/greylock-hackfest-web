651de0058a42f14c7e8b5869ba029bb3eda99ae4
Allen Park <apark@pandora.com>
2014-07-19 13:44:44 -0700

    basic index page

--- wave_web/urls.py
+++ wave_web/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from wave_web.views import *
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'wave_web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', wave_admin),
+    url(r'^admin/$', wave_admin),
+    url(r'^$', index)
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- wave_web/views.py
+++ wave_web/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
 def wave_admin(request):
-	return render_to_response("admin.html", {}, RequestContext(request))
+    return render_to_response("admin.html", {}, RequestContext(request))
+
+def index(request):
+    values = {}
+    values['name'] = "Will Jamieson"
+    values['num_users'] = 10
+    return render_to_response("index.html", values, RequestContext(request))
