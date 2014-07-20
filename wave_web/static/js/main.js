75e3b3c093d71fd56773a903a3a144ac296c6d42
Allen Park <apark@pandora.com>
2014-07-19 19:42:33 -0700

    map working

--- wave_web/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.message-input').bind('input', function(e) {
-        var text = $('.message-input').val();
-        if (text === '') {
-            text = '&nbsp;';
-        }
-        $('.preview').html(text);
-    });
-});
